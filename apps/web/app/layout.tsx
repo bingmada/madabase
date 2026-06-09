869d46ebeebd5543c7ec0db5200b8011286007b0
15797688584@163.com <liyingda@mac-6.local>
2026-06-09 17:29:20 +0800

    adsense

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -24,12 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4491218467179399"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        <meta name="google-adsense-account" content="ca-pub-4491218467179399" />
       </head>
       <body className={`${inter.variable} ${jetbrainsMono.variable}`}>
         {children}
